926b1dcc6d06b3a6a12120a22996329e445846f5
gridwork-nl <info@gridwork.nl>
2021-03-22 17:03:22 +0100

    fix(server): removed prisma from git

---
+++ server/src/Activity.ts
@@ -0,0 +1,14 @@
+import 'reflect-metadata';
+import {ObjectType, Field, ID } from 'type-graphql';
+
+@ObjectType()
+export class Activity {
+  @Field(type => ID)
+  id: number;
+
+  @Field()
+  description: string;
+
+  @Field(type => [String])
+  tags: string[];
+}

827139e3c8d322d8e3d419c08fee4f1868b50c75
gridwork-nl <info@gridwork.nl>
2021-03-23 19:21:09 +0100

    feat(server/user): updated user resolvers

--- server/src/User/UserResolvers.ts
+++ server/src/User/UserResolvers.ts
@@ -1,30 +1,62 @@
 import 'reflect-metadata';
-import { Query, Mutation, Arg, Ctx, FieldResolver, Root, Int, Field, InputType, Resolver} from 'type-graphql';
-import { Prisma } from '@prisma/client';
-import {User} from './User';
-import { Context} from '../context';
+import {
+  Query,
+  Mutation,
+  Arg,
+  Ctx,
+  Int,
+  Field,
+  InputType,
+  Resolver,
+  ID,
+} from 'type-graphql';
+import { prisma, Prisma } from '@prisma/client';
+import { User } from './User';
+import { Context } from '../context';
 import { Activity } from '../Activity/Activity';
 
 @InputType()
 class AddUserInput {
   @Field()
-  name: string
+  name: string;
 
   @Field()
-  age: number
-  
-  @Field()
-  gender: string
+  age: number;
 
   @Field()
-  interestedIn: string
+  gender: string;
 
   @Field()
-  location: string
+  interestedIn: string;
+
+  @Field()
+  location: string;
+}
+
+@InputType()
+class RejectUserInput {
+  @Field()
+  ownID: number;
+
+  @Field()
+  rejectedID: number;
 }
 
 @Resolver(User)
 export class UserResolver {
+  // Queries
+
+  // Get user by ID
+  @Query((returns) => User, { nullable: true })
+  async user(@Arg('id', (type) => Int) id: number, @Ctx() ctx: Context) {
+    return ctx.prisma.user.findUnique({
+      where: { id: id },
+    });
+  }
+
+  // Mutations
+
+  // Add new user
   @Mutation((returns) => User)
   async addUser(
     @Arg('data') data: AddUserInput,
@@ -41,12 +73,27 @@ export class UserResolver {
     });
   }
 
-  @Query((returns) => User, { nullable: true })
-  async user(@Arg('id', (type) => Int) id: number, @Ctx() ctx: Context) {
-    return ctx.prisma.user.findUnique({
-      where: { id: id },
+  // Add user to rejection list
+  @Mutation((returns) => User)
+  async rejectUser(
+    @Arg('data') data: RejectUserInput,
+    @Ctx() ctx: Context
+  ): Promise<User> {
+    // Query user array that the current user rejected
+    const res = await ctx.prisma.user.findUnique({
+      where: { id: data.ownID },
+      select: {
+        rejections: true,
+      },
+    });
+    // Update that array with the newly rejected user ID
+    return await ctx.prisma.user.update({
+      data: {
+        rejections: {
+          set: [...res.rejections, data.rejectedID],
+        },
+      },
+      where: { id: data.ownID },
     });
   }
 }
-
-

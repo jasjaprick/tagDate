2a56e804ec87e02eaacf5daab0b843f9903be74f
gridwork-nl <info@gridwork.nl>
2021-03-24 16:10:47 +0100

    refactor(server/seed): removed double quotes

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -60,31 +60,31 @@ const userData: Prisma.UserCreateInput[] = [
 ];
 
 const activityData: Prisma.ActivityUncheckedCreateInput[] = [
- { description : "drinking beers until we vomit",
- tag: "beers",
+ { description : 'drinking beers until we vomit',
+ tag: 'beers',
  postedBy: 1 ,
  },
- { description : "going to the cinema and commit mass shooting",
- tag: "fun",
+ { description : 'going to the cinema and commit mass shooting',
+ tag: 'fun',
  postedBy: 2 ,
  },
- { description : "i will accept anything cause i am very sad",
- tag: "sadness",
+ { description : 'i will accept anything cause i am very sad',
+ tag: 'sadness',
  postedBy: 3 ,
  },
- { description : "playing with dolls cause that is a very comon thing to do on a first date",
- tag: "dolls",
+ { description : 'playing with dolls cause that is a very comon thing to do on a first date',
+ tag: 'dolls',
  postedBy: 4,
  },
- { description : "whatever",
- tag: "whatever",
+ { description : 'whatever',
+ tag: 'whatever',
  postedBy: 5,
  },
- { description : "playing football",
- tag: "football",
+ { description : 'playing football',
+ tag: 'football',
  postedBy: 6
  }
-]
+];
 
 async function main() {
   console.log('Start seeding');
@@ -98,7 +98,7 @@ async function main() {
   for (const a of activityData) {
     const activity = await prisma.activity.create({
       data: a
-    })
+    });
     console.log(`Created activity with id ${activity.id}`);
   }
 }

456fa6b3caead6e1c73a0cfe0e2f1946e394dab4
gridwork-nl <info@gridwork.nl>
2021-03-25 23:22:52 +0100

    feat(client/chat): added chat user name and random message

--- client/components/atoms/Chat.tsx
+++ client/components/atoms/Chat.tsx
@@ -6,22 +6,41 @@ interface IProps {
 }
 
 const Chat: React.FunctionComponent<IProps> = ({match})  => {
+  // Retrieving both users from the match prop, where on is loggin in user and other is target user
   const { userOne, userTwo } = match;
 
+  //TODO: TS def
+  // Set empty var as user that is the target user that we want to display
   let userToDisplay;
 
+  // filter for the target user and display that user in the chat
   if (userOne.id === 1) {
     userToDisplay = userTwo;
   } else {
     userToDisplay = userOne;
   }
 
+  // Set array of random messages
+  //TODO: remove and replace with chat function when it is ready
+    const randomMessages = [
+    'Hello, how are you?',
+    'Let\'s meet up later!',
+    'Cool that you also want to go fishing!',
+    'Our date was amazing!'
+  ];
+
+  // set fn that gets a random number between 0-3 and retrieve a random message to display
+  const getRandomMessageIndex = () => {
+    return Math.floor(Math.random() * Math.floor(4));
+  };
+
+
   return (
     <View style={styles.chatContainer}>
       <View style={styles.photoContainer}></View>
       <View style={styles.infoContainer}>
         <Text style={styles.chatInfoName}>{userToDisplay.profile.name}</Text>
-        <Text>Hello, how are you?</Text>
+        <Text>{randomMessages[getRandomMessageIndex()]}</Text>
       </View>
     </View>
   );

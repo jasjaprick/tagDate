75fe43fd5906eb62f9c6a16bdf57b71bfd7bcf44
Salvatore Pariota <salpariota@gmail.com>
2021-03-25 18:49:37 +0100

    feat(server): implemented letter gang by tag

--- server/prisma/mockData/mockUsers.ts
+++ server/prisma/mockData/mockUsers.ts
@@ -25,4 +25,31 @@ export const userData: Prisma.UserCreateInput[] = [
     email: 'lulu@example.com',
     password: 'lulu',
   },
+  {
+    email: 'gino@example.com',
+    password: 'gino',},
+{
+  email: 'yanira@example.com',
+  password: 'gino',
+  },
+  {
+    email: 'giorgio@example.com',
+    password: 'giorgio',
+  },
+  {
+    email: 'jorge@example.com',
+    password: 'jorge',
+  },
+  {
+    email: 'mark@example.com',
+    password: 'mark',
+    },
+    {
+      email: 'lola@example.com',
+    password: 'lola',
+    },
+    {
+      email: 'francesca@example.com',
+    password: 'francesca',
+    }
 ];

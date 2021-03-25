44d5cf9a46ba3e637c7b5db4fb6935f916367684
gridwork-nl <info@gridwork.nl>
2021-03-26 00:50:21 +0100

    feat(client): added swipe user logic from backend

--- client/components/pages/SwipePage.tsx
+++ client/components/pages/SwipePage.tsx
@@ -2,34 +2,68 @@ import React, { useEffect, useState } from 'react';
 import { dbUser, IUsers } from '../../db';
 import { Text, View } from 'react-native';
 import Swipe from '../organisms/Swipe';
+import {useQuery, useMutation, gql} from '@apollo/client';
+import QueryResult from '../organisms/QueryResult';
 
 interface Props {
   dbUser: IUsers[]
 }
 
+// GraphQL definitions
+const GET_MATCHING_ACTIVITIES = gql`
+  query Query($ownId: Float!, $tag: String!) {
+    findActivityByTag(
+      ownID: $findActivityByTagOwnId
+      tag: $findActivityByTagTag
+    ) {
+      description
+      tag
+      user {
+        profile {
+          name
+          bio
+          age
+          location
+          profilePicture
+        }
+      }
+    }
+  }
+`;
+
+const LIKE_USER = gql`
+  mutation Mutation($likeData: AddPossibleMatchInput!) {
+    addPossibleMatch(data: $likeData) {
+      id
+    }
+  }
+`;
+
 const SwipePage: React.FunctionComponent<Props> = () => {
   const [index, setIndex]  = useState(0);
-  const [user, setUser]  = useState<IUsers>(dbUser[0]);
+
+  const {loading, error, data } = useQuery(GET_MATCHING_ACTIVITIES);
+  const [likeUser] = useMutation(LIKE_USER);
   
-  useEffect(() => {
-    setUser({...dbUser[index]});
-  }, [index]);
-  console.log(user, 'USER');
-  const onLike = () => {setIndex(index+1);};
+  // If error in fetching data then console.log error
+  if(error) console.log(error);
+
+  const onLike = () => {
+    likeUser({variables: {likeData: {UID1: 1, UID2: data.postedBy}}});
+    setIndex(index+1);
+  };
   const onNoLike = () => {setIndex(index+1);};
 
   return (
-    <View>
-      {dbUser[index] ? 
-        <Swipe 
-        user={user}
-        onLike={onLike}
-        onNoLike={onNoLike}
-        />
-        :
-        <Text>NO MORE USERS!</Text>
-      }
-    </View>
+    <QueryResult error={error} loading={loading} data={data}>
+      <View>
+        {data[index] ? (
+          <Swipe target={data[index]} onLike={onLike} onNoLike={onNoLike} />
+        ) : (
+          <Text>NO MORE USERS!</Text>
+        )}
+      </View>
+    </QueryResult>
   );
 };
 

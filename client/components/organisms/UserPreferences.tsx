c3e265933931d32a851c79fa51fbec14f435b9a3
yanira <yanira.p@hotmail.com>
2021-03-26 08:27:52 +0100

    style(UserPregerence):Added gender laber

--- client/components/organisms/UserPreferences.tsx
+++ client/components/organisms/UserPreferences.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { RadioButton } from 'react-native-paper';
+import { yellow100 } from 'react-native-paper/lib/typescript/styles/colors';
 
 import colors from '../../helpers/colors';
 
@@ -28,16 +29,25 @@ const UserPreferences: React.FC<IPropsPreferences> = ({
     <View>
       <View style={styles.genderContainer}>
         <Text style={styles.font}>Interested in</Text>
-        <RadioButton
-          value='male'
-          status={genderPreference === 'male' ? 'checked' : 'unchecked'}
-          onPress={() => setGenderPreference('male')}
-        />
-        <RadioButton
-          value='female'
-          status={genderPreference === 'female' ? 'checked' : 'unchecked'}
-          onPress={() => setGenderPreference('female')}
-        />
+
+        <View style={styles.iconContainer}>
+          <View>
+            <RadioButton
+              value='male'
+              status={genderPreference === 'male' ? 'checked' : 'unchecked'}
+              onPress={() => setGenderPreference('male')}
+            />
+            <Text style={styles.fontBtn}>Male</Text>
+          </View>
+          <View>
+            <RadioButton
+              value='female'
+              status={genderPreference === 'female' ? 'checked' : 'unchecked'}
+              onPress={() => setGenderPreference('female')}
+            />
+            <Text style={styles.fontBtn}>Female</Text>
+          </View>
+        </View>
       </View>
 
       <>
@@ -70,6 +80,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   font: { fontSize: 20, color: colors.grey },
+  fontBtn: { fontSize: 12, color: colors.grey },
+  iconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    marginLeft: '3%',
+    flex: 1,
+  },
 });
 
 export default UserPreferences;

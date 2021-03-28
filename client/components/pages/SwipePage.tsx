0686093c2b9f412afe5af0be6bec49f15df80e6c
Mathias Soderqvist <mathias.soderqvist@live.com>
2021-03-28 15:29:32 +0200

    feat(Swipe): Swipe working

--- client/components/pages/SwipePage.tsx
+++ client/components/pages/SwipePage.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { dbUser, IUsers } from '../../db';
-import { Text, View } from 'react-native';
+import { Text, View,  SafeAreaView, Dimensions } from 'react-native';
 import Swipe from '../organisms/Swipe';
 import {useQuery, useMutation, gql} from '@apollo/client';
 import QueryResult from '../organisms/QueryResult';
@@ -48,6 +48,10 @@ mutation RejectUserMutation($ownId: Float!, $rejectedId: Float!) {
 
 const SwipePage: React.FunctionComponent<Props> = () => {
   const [index, setIndex]  = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
+ 
+  const [likeUser] = useMutation(LIKE_USER);
+  const [rejectUser] = useMutation(REJECT_USER);
 
   const {loading, error, data } = useQuery(GET_MATCHING_ACTIVITIES, {
     variables: {
@@ -55,34 +59,61 @@ const SwipePage: React.FunctionComponent<Props> = () => {
       tag: 'fish'
     }
   });
+  const [carouselItems, setCarouslItems] = useState([]);
 
-  const [likeUser] = useMutation(LIKE_USER);
-  const [rejectUser] = useMutation(REJECT_USER);
+  useEffect(() => {
+    // console.log("DATA", data.findActivityByTag);
+    if (data?.findActivityByTag){
+      setCarouslItems(data.findActivityByTag);
+    }
+  }, [data]);
+
+  const windowWidth = Math.round(Dimensions.get('window').width); 
 
   const onLike = () => {
-    likeUser({variables: {likeData: {UID1: 1, UID2: data.findActivityByTag[index].postedBy}}});
+    const UID2 = data?.findActivityByTag[index] ? data.findActivityByTag[index].postedBy : 0;
+    likeUser({variables: {likeData: {UID1: 1, UID2 }}});
     setIndex(index+1);
   };
   const onNoLike = () => {
-    rejectUser({variables: {ownId: 1, rejectedId: data.findActivityByTag[index].postedBy}});
+    const rejectedId = data?.findActivityByTag[index] ? data.findActivityByTag[index].postedBy : 0;
+    rejectUser({variables: {ownId: 1, rejectedId }});
     setIndex(index+1);};
 
+    const  _renderItem = ({item, indexRender}) => {
+      
+      return (
+        <QueryResult error={error} loading={loading} data={data}>
+        <View>
+          {
+           item ? 
+            (
+              <Swipe
+                target={item}
+                onLike={onLike}
+                onNoLike={onNoLike}
+              />
+              ) : (
+                <Text>NO MORE USERS!</Text>
+              )
+          }
+             </View>
+       </QueryResult>
+      );
+  }; 
+
   return (
-    <QueryResult error={error} loading={loading} data={data}>
-      <View>
-        {data &&
-        data.findActivityByTag.length >= 1 &&
-        index < data.findActivityByTag.length ? (
-          <Swipe
-            target={data.findActivityByTag[index]}
-            onLike={onLike}
-            onNoLike={onNoLike}
-          />
-        ) : (
-          <Text>NO MORE USERS!</Text>
-        )}
-      </View>
-    </QueryResult>
+    <View style={{flex: 1 }}>
+    <View style={{ flex: 1, flexDirection:'row', justifyContent: 'center', }}>
+        <Carousel
+          layout={"tinder"}
+          data={carouselItems}
+          sliderWidth={300}
+          itemWidth={windowWidth}
+          renderItem={_renderItem}
+          onSnapToItem = { index => setActiveIndex(index) } />
+    </View>
+  </View>
   );
 };
 

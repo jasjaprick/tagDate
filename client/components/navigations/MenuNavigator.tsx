21c59fc2982e918467403de06cabb5c76b00b633
Jasja Prick <info@gridwork.nl>
2021-03-30 15:46:02 +0200

    Update MenuNavigator.tsx

--- client/components/navigations/MenuNavigator.tsx
+++ client/components/navigations/MenuNavigator.tsx
@@ -21,44 +21,4 @@ const MenuNavigator = () => {
   );
 };
 
-// function MenuNavigator() {
-//   return (
-//     <Menu.Navigator
-//       tabBarOptions={{
-//         activeBackgroundColor: colors.violet200,
-//         inactiveBackgroundColor: colors.violet,
-//         activeTintColor: 'white',
-//         inactiveTintColor: 'white',
-//         keyboardHidesTabBar: true,
-//       }}
-//     >
-//       <Menu.Screen
-//         name='Swipe'
-//         component={SwipePage}
-//         options={{
-//           tabBarIcon: () => <AntDesign name='heart' size={24} color='white' />,
-//         }}
-//       />
-//       <Menu.Screen
-//         name='Chat'
-//         component={ChatNavigator}
-//         options={{
-//           tabBarIcon: () => (
-//             <Ionicons name='chatbox-ellipses-sharp' size={24} color='white' />
-//           ),
-//         }}
-//       />
-//       <Menu.Screen
-//         name='Profile'
-//         component={ChatPage}
-//         options={{
-//           tabBarIcon: () => (
-//             <Ionicons name='person-circle-sharp' size={24} color='white' />
-//           ),
-//         }}
-//       />
-//     </Menu.Navigator>
-//   );
-// }
-
 export default MenuNavigator;

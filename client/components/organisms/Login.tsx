04184eed936530a12d2019dd9ff5e63a519cfd78
Jorge Perez <jlpg81@gmail.com>
2021-03-31 11:00:58 +0200

    setting up login

--- client/components/organisms/Login.tsx
+++ client/components/organisms/Login.tsx
@@ -1,18 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import PrimaryButton from '../atoms/PrimaryButton';
 import MainLogo from '../../assets/img/logo-main.svg';
 import { colors } from '../../helpers/styles';
 import InputFieldShort from '../atoms/InputFieldShort';
+import { gql, useLazyQuery } from '@apollo/client';
+
+const LOGIN = gql`
+  query Query($email: String!, $password: String!) {
+    getWebToken(email: $email, password: $password) {
+      accessToken
+    }
+  }
+`;
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigation = useNavigation();
   const handleLogin = () => {
-    navigation.replace('MenuNavigator');
+    // navigation.replace('MenuNavigator');
+    console.log(`clicking login`);
+
+    login({
+      variables: {
+        email: 'diana@example.com',
+        password: '1234',
+      },
+    });
   };
+
+  // console.log('username', username, 'password', password);
+
+  const [login, { called, loading, data }] = useLazyQuery(LOGIN);
+
+  console.log(`data`, data);
+
+  useEffect(() => {
+    console.log(`data`, data);
+    if (loading) {
+      console.log(`Loading!!`);
+    }
+    if (data) {
+      navigation.navigate('MenuNavigator');
+    }
+    if (data === null) {
+      console.log(`invalid username`);
+    }
+  }, [data]);
 
   return (
     <View style={styles.loginContainer}>
